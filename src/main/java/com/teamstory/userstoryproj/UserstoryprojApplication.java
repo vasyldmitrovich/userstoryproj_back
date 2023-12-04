e3c1b4d7b3d2d696285f882cc8e0f50a642dec14
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-12-04 23:46:13 +0200

    add project structure

---
+++ src/main/java/com/teamstory/userstoryproj/UserstoryprojApplication.java
@@ -0,0 +1,13 @@
+package com.teamstory.userstoryproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserstoryprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserstoryprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamstory/userstoryproj/UserstoryprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamstory.userstoryproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserstoryprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
