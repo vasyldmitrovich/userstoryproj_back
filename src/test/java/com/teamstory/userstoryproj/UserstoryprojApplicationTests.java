7dd9beb7b663a62191af7ecbeb97004cf5eb1b2a
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-12-16 14:23:42 +0200

    change property and test class

--- src/test/java/com/teamstory/userstoryproj/UserstoryprojApplicationTests.java
+++ src/test/java/com/teamstory/userstoryproj/UserstoryprojApplicationTests.java
@@ -3,11 +3,12 @@ package com.teamstory.userstoryproj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = UserstoryprojApplicationTests.class)
 class UserstoryprojApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Test have finished");
     }
 
 }
