f6e874a1454505cfb16964c7af0f9568c0a75da1
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2025-03-17 21:22:35 +0200

    Switch to H2 db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,10 @@
-INSERT INTO projects (id, description, name) VALUES (3, 'Description about project1', 'Project1');
+INSERT INTO projects (description, name) VALUES ('Description project1', 'Project1');
+INSERT INTO projects (description, name) VALUES ('Description project2', 'Project2');
+INSERT INTO projects (description, name) VALUES ('Description project3', 'Project3');
+INSERT INTO projects (description, name) VALUES ('Description project4', 'Project4');
+INSERT INTO projects (description, name) VALUES ('Description project5', 'Project5');
+INSERT INTO projects (description, name) VALUES ('Description project6', 'Project6');
+INSERT INTO projects (description, name) VALUES ('Description project7', 'Project7');
+INSERT INTO projects (description, name) VALUES ('Description project8', 'Project8');
+INSERT INTO projects (description, name) VALUES ('Description project9', 'Project9');
+INSERT INTO projects (description, name) VALUES ('Description project10', 'Project10');
