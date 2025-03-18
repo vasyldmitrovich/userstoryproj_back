2d4ee9457cb594e49014a56d18dbe58f2f76f36c
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2025-03-18 15:09:33 +0200

    Get user data from properties

--- src/main/java/com/teamstory/userstoryproj/config/SecurityConfig.java
+++ src/main/java/com/teamstory/userstoryproj/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.teamstory.userstoryproj.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${app.user.username}")
+    private String username;
+
+    @Value("${app.user.password}")
+    private String password;
+
+    @Value("${app.user.roles}")
+    private String roles;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -43,9 +53,9 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = User.builder()
-            .username("admin")
-            .password(passwordEncoder.encode("password")) // Password stored in encrypted form
-            .roles("USER")
+            .username(username)
+            .password(passwordEncoder.encode(password)) // Password stored in encrypted form
+            .roles(roles.split(","))
             .build();
 
         return new InMemoryUserDetailsManager(user);
